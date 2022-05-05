a32ae41e2b09f295dddb23640eb6dc49ed16de0e
Doug Beckwith <dougcbeckwith@gmail.com>
2022-05-05 11:53:06 -0700

    finished going back to no usecontext

--- src/pages/Coin.jsx
+++ src/pages/Coin.jsx
@@ -9,25 +9,18 @@ const Coin = () => {
   const [coin, setCoin] = useState([])
 
   const params = useParams()
-  console.log(params.id)
 
   useEffect(() => {
-    console.log('effect here')
-    console.log(params.id)
     getCoin(params.id)
   }, [])
 
   const getCoin = async (coinid) => {
-    console.log('start get coin')
     const response = await axios.get(
       `https://api.coingecko.com/api/v3/coins/${coinid}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false`
     )
-    console.log(response.data)
     setCoin(response.data)
     setIsCoinLoading(false)
-    console.log('end get coin')
   }
-  console.log('end of file')
   return (
     <>
       {coinLoading && (
