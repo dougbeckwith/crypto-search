8f8e0af66da10ee5117f481b745dc129fb739fa3
Doug Beckwith <dougcbeckwith@gmail.com>
2022-05-05 12:37:37 -0700

    updated styles

--- src/components/Coins/CoinsTable.jsx
+++ src/components/Coins/CoinsTable.jsx
@@ -11,13 +11,12 @@ const Coins = ({filteredCoins}) => {
               <tr className='border-b text-left'>
                 <th className='pl-2'>#</th>
                 <th>Name</th>
-                <th className='hidden lg:table-cell'>Ticker</th>
-                <th className='pr-2 text-right'>Price CAD</th>
-                <th className='text-right hidden lg:table-cell'>Volume</th>
-                <th className='text-right hidden 2xl:table-cell'>24H %</th>
-                <th className='text-right hidden md:table-cell pr-2'>
-                  Marketcap
-                </th>
+                <th className='text-right hidden'>Ticker</th>
+                <th className='text-right hidden'>Price CAD</th>
+                <th className='text-right hidden'>Volume</th>
+                <th className='text-right hidden'>24H %</th>
+                <th className='text-right hidden'>Marketcap</th>
+                <th className='text-right pr-2'>Links</th>
               </tr>
             </thead>
             <tbody>
