dc6c467aa94eea1760e7dd754919175560bd8bb7
Doug Beckwith <dougcbeckwith@gmail.com>
2022-06-10 19:11:11 -0700

    add / delete watchlist

--- src/components/WatchList/CoinCard.jsx
+++ src/components/WatchList/CoinCard.jsx
@@ -1,54 +1,60 @@
 import React from 'react'
+import {CgClose} from 'react-icons/cg'
 
-const CoinCard = ({coin}) => {
-  console.log(coin)
+const CoinCard = ({coin, handleDelete}) => {
   return (
-    <div>{coin.id}</div>
-    // <div className='w-full rounded-md py-3 px-3 mb-2 bg-[#f7f7f5] flex items-center justify-between'>
-    //   <div>
-    //     <img className='w-20' src={coin.image.large} alt='' />
-    //   </div>
-    //   <div className=''>
-    //     <p className='text-xl'>
-    //       {coin.id.charAt(0).toUpperCase() + coin.id.slice(1)}
-    //     </p>
-    //     <p className='text-lg'>{coin.symbol.toUpperCase()}</p>
-    //   </div>
-    //   <div className=''>
-    //     <p className='text-xl'>Price</p>
-    //     <p className='text-lg'>
-    //       ${coin.market_data.current_price.cad.toLocaleString('en-US')}
-    //     </p>
-    //   </div>
-    //   <div className='hidden sm:block'>
-    //     <p className='text-xl'>24H %</p>
-    //     <p
-    //       className={
-    //         coin.market_data.price_change_percentage_24h_in_currency.cad >= 0
-    //           ? 'text-lg  text-[green] '
-    //           : 'text-lg  text-[red]'
-    //       }>
-    //       {coin.market_data.price_change_percentage_24h_in_currency.cad.toFixed(
-    //         2
-    //       )}
-    //     </p>
-    //   </div>
-    //   <div className='hidden lg:block'>
-    //     <p className='text-xl'>ATH</p>
-    //     <p className='text-lg'>${coin.market_data.ath.cad.toLocaleString()}</p>
-    //   </div>
-    //   <div className='hidden lg:block'>
-    //     <p className='text-xl'>ATH %</p>
-    //     <p
-    //       className={
-    //         coin.market_data.ath_change_percentage.cad >= 0
-    //           ? 'text-right text-lg  text-[green] '
-    //           : 'text-right text-lg  text-[red]'
-    //       }>
-    //       {coin.market_data.ath_change_percentage.cad.toFixed(2)}
-    //     </p>
-    //   </div>
-    // </div>
+    <div className='shadow-lg w-full rounded-md py-3 px-3 mb-2 bg-[#f7f7f5] flex items-center justify-between'>
+      <div className='w-full flex items-center justify-between'>
+        <div>
+          <img className='w-20' src={coin.image} alt='' />
+        </div>
+        <div className=''>
+          <p className='hidden sm:block text-xl'>
+            {coin.id.charAt(0).toUpperCase() + coin.id.slice(1)}
+          </p>
+          <p className='text-lg'>{coin.symbol.toUpperCase()}</p>
+        </div>
+        <div className=''>
+          <p className='text-xl hidden sm:block'>Price</p>
+          <p className='text-lg'>
+            ${coin.current_price.toLocaleString('en-US')}
+          </p>
+        </div>
+        <div className='hidden sm:block'>
+          <p className='text-xl'>24H %</p>
+          <p
+            className={
+              coin.price_change_percentage_24h >= 0
+                ? 'text-lg  text-[green] '
+                : 'text-lg  text-[red]'
+            }>
+            {coin.price_change_percentage_24h.toFixed(2)}
+          </p>
+        </div>
+        <div className='hidden lg:block'>
+          <p className='text-xl'>ATH</p>
+          <p className='text-lg'>${coin.ath.toLocaleString()}</p>
+        </div>
+        <div className='hidden lg:block'>
+          <p className='text-xl'>ATH %</p>
+          <p
+            className={
+              coin.ath_change_percentage >= 0
+                ? 'text-right text-lg  text-[green] '
+                : 'text-right text-lg  text-[red]'
+            }>
+            {coin.ath_change_percentage.toFixed(2)}
+          </p>
+        </div>
+        <div className='border-2 border-transparent hover:cursor-pointer hover:border-2 hover:border-[#5e61fa] hover:rounded'>
+          <CgClose
+            size={30}
+            color='#5e61fa'
+            onClick={() => handleDelete(coin)}
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/WatchList.jsx
+++ src/pages/WatchList.jsx
@@ -1,29 +1,53 @@
 import React from 'react'
 import {useState, useEffect} from 'react'
 import CoinCard from '../components/WatchList/CoinCard'
+import {v4 as uuidv4} from 'uuid'
 
 const WatchList = () => {
   const [watchListCoins, setWatchListCoins] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [reRender, setReRender] = useState(true)
+
   useEffect(() => {
     const setData = async () => {
       const coins = await JSON.parse(localStorage.getItem('watchListCoins'))
       setWatchListCoins(coins)
       setIsLoading(false)
-      console.log('here')
-      console.log(coins)
     }
     setData()
-  }, [])
+  }, [reRender])
+
+  function saveDataToLocalStorage(coin) {
+    let a = []
+    a = JSON.parse(localStorage.getItem('watchListCoins'))
+    let updateA = a.filter((item) => {
+      return item.id !== coin.id
+    })
+
+    localStorage.setItem('watchListCoins', JSON.stringify(updateA))
+    setReRender(!reRender)
+  }
+
+  const handleDelete = (coin) => {
+    saveDataToLocalStorage(coin)
+  }
 
   return (
     <>
-      <h1>WatchList</h1>
-      {!isLoading &&
-        watchListCoins.map((coin) => {
-          console.log(coin)
-          return <CoinCard coin={coin} />
-        })}
+      <div className='w-full bg-[#f7f7f5] min-h-screen max-h-min '>
+        <div className='container m-auto pt-10'>
+          {!isLoading &&
+            watchListCoins.map((coin) => {
+              return (
+                <CoinCard
+                  key={uuidv4()}
+                  coin={coin}
+                  handleDelete={handleDelete}
+                />
+              )
+            })}
+        </div>
+      </div>
     </>
   )
 }
